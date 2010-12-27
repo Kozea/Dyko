5a660e501853c5178ef4e19dd299ce3148a7112d
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2010-12-27 16:10:08 +0100

    Use the default theme for API

--- api/conf.py
+++ api/conf.py
@@ -3,6 +3,6 @@ release = '0.2'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 html_favicon = '../static/css/img/icon.png'
-html_theme = 'sphinx'
+html_theme = 'default'
 html_theme_path = ['../../..']
 master_doc = 'index'
